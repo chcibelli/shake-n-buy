83c1eff2a60a74b0607fef442fdddc71b9b136c1
Christian Cibelli <chcibelli@gmail.com>
2021-10-16 20:01:37 -0300

    varios

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ $( document ).ready(function() {
             return false;
         }
         
-        let url = 'https://api.mercadolibre.com/sites/'+site+'/search?valor='+tope.trim();
-        
+        let url = 'https://api.mercadolibre.com/sites/'+site+'/search?q='+tope.trim();
+
         $.getJSON(url, function(data) {
             
             if (typeof data.seller === "undefined") {
                 $('#result').html('<p> No encontre nada 😢</p>');
                 $('#share').html('');
-                $('#share').hide();    
+                $('#share').hide();
                 $('#result').show();
             } else {
                 let result = '<p>XXXX</p>';
@@ -46,11 +46,38 @@ $( document ).ready(function() {
     $('#tope').on('keypress',function(e) {
         if(e.which == 13) {
             $('#main-btn').click();
+            return false;
         }
+        if (e.which > 31 && (e.which < 48 || e.which > 57)) {
+            return false;
+        }
+        
+        return true;
     });
 
-    $("#tope").inputFilter(function(value) {
-        return /^\d*$/.test(value);
-      });
-  
+    var availableSites = [ 
+        {label:"Argentina 🇦🇷", value:"MLA"}, 
+        {label:"Bolivia 🇧🇴",value:"MBO"},
+        {label:"Brasil 🇧🇷", value:"MLB"},
+        {label:"Chile 🇨🇱", value:"MLC"},
+        {label:"Colombia 🇨🇴", value:"MCO"},
+        {label:"Costa Rica 🇨🇷", value:"MCR"},
+        {label:"Cuba 🇨🇺", value:"MCU"},
+        {label:"Dominicana 🇩🇴", value:"MRD"},
+        {label:"Ecuador 🇪🇨", value:"MEC"},
+        {label:"Guatemala 🇬🇹", value:"MGT"},
+        {label:"Honduras 🇭🇳", value:"MHN"},
+        {label:"Mexico 🇲🇽", value:"MLM"},
+        {label:"Nicaragua 🇳🇮", value:"MNI"},
+        {label:"Panama 🇵🇦", value:"MPA"},
+        {label:"Paraguay 🇵🇾", value:"MPY"},
+        {label:"Perú 🇵🇪", value:"MPE"},
+        {label:"Uruguay 🇺🇾", value:"MLU"},
+        {label:"Venezuela 🇻🇪", value:"MLV"}
+    ];
+
+    for(t=0;t<availableSites.length;t++) {
+        $("#hidden_site").append(new Option(availableSites[t].label, availableSites[t].value));
+    }    
+
 });
